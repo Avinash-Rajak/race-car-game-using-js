ca9e27318d446ac488b47c33b7acddc003bfd9c4
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-17 12:06:37 +0530

    learning

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function countdown (){
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
+// function countdown() {
+//   console.log(5);
+//   console.log(4);
+//   console.log(3);
+//   console.log(2);
+//   console.log(1);
+// }
+
+// setting up the race
+
+// countdown()
+
+
+// Go 
+// players are runnig the race 
+// race gets over 
+// new race starts 
+
+// countdown();
+
+
+
+
+// function learning (){
+//     console.log(42)
+// }
+
+// learning ()
+
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function totallaptime(){
+    let totaltime = lap1 + lap2 + lap3;
+    console.log(totaltime)
 }
 
-// setting up the race 
-
-countdown()
+totallaptime()
